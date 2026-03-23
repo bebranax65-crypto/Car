3d52fe18bd558a0416373fa8a8cb80166e5e51f7
Artsiom Pasadski <posadski28@gmail.com>
2026-03-23 18:14:03 +0300

    Мой первый коммит

---
+++ Car/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApp2
+{
+
+    class car
+    {
+        public string brand;
+        public int FoundationYear;
+        public car()
+        {
+            brand = "audi";
+            FoundationYear = 2000;
+        }
+        static void Main(string[] args)
+        {
+            car mycar = new car();
+            mycar.Print();
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Бренд: {brand} Дата выпуска: {FoundationYear} ");
+        }
+    }
+}
